f3a4fa856a58a933b2a8173c69361a6ea1931cd0
mrwhiz <mrjohnsonjr@outlook.com>
2015-06-13 22:54:14 -0400

    Start

---
+++ xPlatAuction.Backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="xPlatAuction.Backend.WebApiApplication" Language="C#" %>
---
+++ xPlatAuction/xPlatAuction.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using xPlatAuction.WinPhone.Resources;
+
+namespace xPlatAuction.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
